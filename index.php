c03fcd009f19344fe9e6462a82d9e27b22d791e8
Jiro Lin <jiro.lin9611@gmail.com>
2015-03-16 09:05:51 +0800

    this is new cal format
    Signed-off-by: Jiro Lin <jiro.lin9611@gmail.com>

--- index.php
+++ index.php
@@ -1,55 +1,136 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
+<head><title>PHP/CSS Calculator</title>
+<meta charset="UTF-8">
+<link href="css/basic.css" rel="stylesheet" type="text/css" />
 <head>
-	<meta charset="UTF-8">
-	<title>PHP ReactJS Calculator</title>
-</head>
 <body>
-	<form action="index.php" method="POST">
-		<div class="enter_area">
-			<p>PHP ReactJS Calculator By JIRO</p>
-			<input id="out-put" type="text" readonly="readonly" name="" value="" placeholder="Please enter number...">
-			<?php
-				if(empty($_POST['jiro_test'])){
-					$jiro_test = " ";
-				}else{
-					$jiro_test = $_POST['jiro_test'];
-					echo "jiro_test: ".$jiro_test."<br>";
-				}
-			?>
-		</div>
-		<div class="keys">
-			<p>
-				<input type="text" name="jiro_test">
-				<input type="submit" class="num_val" name="num1" value="1">
-				<input type="submit" class="num_val" name="num2" value="2">
-				<input type="submit" class="num_val" name="num3" value="3">
-				<input type="submit" name="add" value=" + ">
+<div id="calc">
+<div id="header">Jiro Practice Calculator
+	<div id="closeBtn"></div>
+</div>
 
-			</p>
-			<p>
-				<input type="submit" class="num_val" name="num4" value="4">
-				<input type="submit" class="num_val" name="num5" value="5">
-				<input type="submit" class="num_val" name="num6" value="6">
-				<input type="submit" name="minus" value=" - ">
-			</p>
-			<p>
-				<input type="submit" class="num_val" name="num7" value="7">
-				<input type="submit" class="num_val" name="num8" value="8">
-				<input type="submit" class="num_val" name="num9" value="9">
-				<input type="submit" name="sum" value=" x ">
-			</p>
-			<p>
-				<input type="submit" class="num_val" name="num0" value="0">
-				<input type="submit" class="num_val" name="num_dot" value=".">
-				<input type="submit" class="num_val" name="num_c" value="C">
-				<input type="submit" name="divide" value=" / ">
-			</p>
-			<P>
-				<input type="submit" name="result_post" value="=" style="width: 110px;">
-			</P>
-		</div>
+<?php
+
+$store_op=$_POST['lastOp'];
+
+for ($i=0;$i<18;$i++){
+
+	//$_POST[$i] is the value of the input name.
+	//SO, if the $i = 13, which means the post get the name="13" value, 
+	//then $_POST[$i] will be "X"
+ 	
+ 	echo "post ".$i.": ".$_POST[$i]."<br>";
+	echo "isset: ".isset($_POST[$i])."<br>";
+	
+	if (isset($_POST[$i])){
+		$currentOp=$i;
+		switch ($currentOp){
+			case 10:
+				$currentOp=' + ';
+			break;
+			case 11:
+				$currentOp=' - ';
+			break;
+			case 12:
+				$currentOp=' / ';
+			break;
+			case 13:
+				$currentOp=' X ';
+			break;
+			case 14:
+				$currentOp="";
+				$store_op=result_get($store_op);
+			break;
+			case 15:
+				$currentOp="";
+				$store_op="";
+			break;
+			case 16:
+				$currentOp=' % ';
+				$store_op=result_get($store_op);
+			break;
+			case 17:
+				$currentOp='.';
+			break;
+
+		}
+	}
+}
+$store_op.=$currentOp;
+
+echo "store_opx: ".$store_op;
+
+function result_get($store_op){
+
+	$opArray=explode(" ", $store_op);
+	var_dump($opArray);
+	$total=$opArray[0];
+
+	$numOfOps=count($opArray);
+	echo "<br>"."numOfOps: ".$numOfOps;
+	for ($i=0;$i<$numOfOps;$i++){
+		$countMod=$i%2;
+		echo " countMod: ".$countMod;
+		if ($countMod == 1){
+			//normally people enter number first, then enter the operator
+			//So 0 1 0 1 0 1 0
+			switch($opArray[$i])
+			{
+				case '+':
+					$total+=$opArray[$i+1];
+				break;
+				case '-':
+					$total-=$opArray[$i+1];
+				break;
+				case 'X':
+					$total*=$opArray[$i+1];
+				break;
+				case '/':
+					$total/=$opArray[$i+1];
+				break;
+				case '%':
+					$total/=100;
+				break;
+			}
+		}
+	}
+return $total;
+}
+
+?>
+
+<div id="formStyle">
+	<form method="post" action="index.php" id="formCalc">
+<?php
+echo <<<_END
+<input type="hidden" value="$store_op" name="lastOp" />
+<input type="textbox" value="$store_op" id="opBox" name="opBox"  /><br>
+_END;
+?>
+		<input type="submit" value="0" id="zero" name="0" class="opButtons" />
+		<input type="submit" value="1" id="one" name="1" class="opButtons" />
+		<input type="submit" value="2" id="two" name="2" class="opButtons" /><br>
+		<input type="submit" value="3" id="three" name="3" class="opButtons" />
+		<input type="submit" value="4" id="four" name="4" class="opButtons" />
+		<input type="submit" value="5" id="five" name="5" class="opButtons" /><br>
+		<input type="submit" value="6" id="six" name="6" class="opButtons" />
+		<input type="submit" value="7" id="seven" name="7" class="opButtons" />
+		<input type="submit" value="8" id="eight" name="8" class="opButtons" /><br>
+		<input type="submit" value="9" id="nine" name="9" class="opButtons" />
+
+		<input type="submit" value="+" id="add" name="10" class="opButtons"/>
+		<input type="submit" value="-" id="sub" name="11" class="opButtons"/><br>
+		<input type="submit" value="/" id="div" name="12" class="opButtons"/>
+		<input type="submit" value="X" id="mul" name="13" class="opButtons"/>
+		<input type="submit" value="=" id="equals" name="14" class="opButtons"/><br>
+		<input type="submit" value="Esc" id="escape" name="15" class="opButtons"/>
+		<input type="submit" value="%" id="percent" name="16" class="opButtons" />
+		<input type="submit" value="." id="dot" name="17" class="opButtons" />
+
+
 	</form>
-
+</div>
+</div>
 </body>
 </html>
